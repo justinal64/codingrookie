1086ff769c08cfe5cb95f6fb510059da557481eb
justin leggett <justin@codingrookie.com>
2018-03-13 20:58:49 -0500

    Added navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import "./App.css";
 import Routes from "./components/Routes";
+import Navigation from "./components/Navigation";
 const logo = require("./logo.svg");
 
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Navigation />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <Routes />
