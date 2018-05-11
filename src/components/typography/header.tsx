07833f6fa6061025ff1a4382b18f14be26f5b766
justin leggett <justin@codingrookie.com>
2018-05-10 22:11:49 -0500

    Added utils folder and added header component

--- src/components/typography/header.tsx
+++ src/components/typography/header.tsx
@@ -1,29 +1,61 @@
 import * as React from "react";
 import styled from "styled-components";
+import { getFontSize } from "../utils/helpers";
 
-// This is just an example of a styled component
 export interface Props {
-  title: string;
+  font?: number; // ? = optional
+  title: string; // no ? means required
+  subtitle?: string;
+  codetext?: string;
 }
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
+const Title = styled.span`
+ ${getFontSize};
+  /* font-size: ${(props: any) => (props.font ? props.font : ".75")}rem; */
 `;
+// const SubTitle = styled.span`
+//   ${getFontSize};
+//   padding-left: 0.25rem;
+// `;
+// const Code = styled.span`
+//   ${getFontSize};
+//   padding-left: 0.25rem;
+//   color: red;
+// `;
 
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
+const addTitle = (title: string, font: any) => {
+  if (title) {
+    return <Title>{title}</Title>;
+  }
+  return 0;
+};
+
+// const addSubTitle = (subtitle: any, font: any) => {
+//   if (subtitle) {
+//     return <SubTitle>{subtitle}</SubTitle>;
+//   }
+//   return 0;
+// };
+
+// const addCode = (codetext: any, font: any) => {
+//   if (codetext) {
+//     return <Code>{codetext}</Code>;
+//   }
+//   return 0;
+// };
 
 class Header extends React.Component<Props, object> {
   public render() {
-    const { title } = this.props;
+    const { font, title } = this.props;
+    {
+      /* subtitle, codetext */
+    }
     return (
-      <Wrapper title={title}>
-        <Title>{title}</Title>
-      </Wrapper>
+      <div className="header">
+        {addTitle(title, font)}
+        {/* {addSubTitle(subtitle, font)} */}
+        {/* {addCode(codetext, font)} */}
+      </div>
     );
   }
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
-// import "./App.css";
-
-// const logo = require("./logo.svg");
+import Header from "../components/typography/header";
 
 class Home extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header title="Options" font={1.25} />
         <p>This is my Home page!!!</p>
       </div>
     );
